051e4699c7a2eed07f87bdf6b1a4e17c903466ae
JMC20003 <105872375+JMC20003@users.noreply.github.com>
2025-04-09 21:42:35 -0500

    first

---
+++ InventarioVentas/src/test/java/com/example/inventarioventas/InventarioVentasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.inventarioventas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventarioVentasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
